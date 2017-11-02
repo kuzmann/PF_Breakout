06f3c5e876ab2ad1c304085a2a59d868dc3a4656
Thomas Glaesser <ty.glaesser@gmail.com>
2017-11-02 15:19:04 +0100

    changed scene size; Brick size depend now on scene size;

--- src/BreakoutMain.java
+++ src/BreakoutMain.java
@@ -2,24 +2,27 @@
  * Created by Tommy on 22.10.2017.
  */
 
-import javafx.application.*;
+import javafx.application.Application;
 import javafx.scene.paint.Color;
-import javafx.stage.*;
-import javafx.scene.*;
-import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 
-public class BreakoutMain extends Application  {
+public class BreakoutMain extends Application {
 
-    static Brick brickSceneSetup = new Brick();
     final static BorderPane root = new BorderPane();
-    final static Scene scene = new Scene(root, brickSceneSetup.getWidth()*10+9*5, brickSceneSetup.getWidth()*10+9*5);
-
+    final static Scene scene = new Scene(root, 640, 640);
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Paddle paddle = Paddle.getInstance();
         Ball ball = Ball.getInstance();
         BrickGrid brickGrid = new BrickGrid();
+
+        /*paddle.setOnKeyPressed((EventHandler<KeyEvent> keyEvent) -> {
+
+         paddle.setX(5.0);
+        })*/
 
         scene.setFill(Color.DARKGRAY);
         root.getChildren().addAll(paddle, ball);
@@ -33,4 +36,6 @@ public class BreakoutMain extends Application  {
     public static void main(String[] args) {
         launch(args);
     }
+
+
 }
